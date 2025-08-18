7b922977ef596d31e7a6f9ae25ec1045f99b2619
abhi <abhiifour@gmail.com>
2025-08-18 21:26:49 +0530

    add post endpoint

--- dist/index.js
+++ dist/index.js
@@ -5,9 +5,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
 app.get("/", (req, res) => {
     res.json({
         message: "hello world!"
+    });
+});
+app.post("/data", (req, res) => {
+    const { name, age } = req.body;
+    res.json({
+        age: age,
+        name: name
     });
 });
 app.listen(3000, () => {
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
+
 
 app.get("/",(req,res) => {
     res.json({
         message:"hello world!"
+    })
+})
+
+app.post("/data", (req,res) =>{
+    const {name, age} = req.body;
+    res.json({
+        age: age,
+        name: name
     })
 })
 
